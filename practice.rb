ead64c999daf4bd83913e5e6e7087bb30f649e56
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-01 16:16:38 -0700

    Add practice problem

---
+++ practice.rb
@@ -0,0 +1,5 @@
+#Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+puts 'type a word: '
+in_word = gets.chomp
+print 'your word in reverse is: ', in_word.reverse, '!'
