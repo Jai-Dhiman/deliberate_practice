83a358d5146c31dfe370f4d29800bde19a13b31b
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-01 16:47:59 -0700

    Update practice.rb

--- practice.rb
+++ practice.rb
@@ -4,3 +4,11 @@ puts 'type a word: '
 in_word = gets.chomp
 print 'your word in reverse is: ', in_word.reverse, '!'
 
+#Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+index = 0
+numbers = [3,6,8,4,7]
+while index < numbers.length
+    puts numbers[index]
+    index=index+1
+end
