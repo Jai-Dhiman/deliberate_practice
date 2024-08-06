66aa434f2e6ff2d36f7675275605a487f385945e
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-05 22:57:59 -0700

    Add excersise solutions

--- practice.rb
+++ practice.rb
@@ -1,14 +1,37 @@
 #Write a program that uses a variable to store a word, then prints that word in reverse order.
 
+=begin
 puts 'type a word: '
 in_word = gets.chomp
 print 'your word in reverse is: ', in_word.reverse, '!'
+=end
 
 #Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
+=begin
 index = 0
 numbers = [3,6,8,4,7]
 while index < numbers.length
     puts numbers[index]
     index=index+1
 end
+
+Write a method that takes in a number and returns the number as a string. Then run the method and print the result.
+Ruby solution
+JavaScript solution
+Python solution
+=end
+
+def method1
+
+num = gets.chomp
+puts num.to_s
+
+end
+
+method1 
+
+
+
+
+
