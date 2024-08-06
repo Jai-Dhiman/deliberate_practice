9eeafe03421be11ad7484a23b4d748841d33cb55
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-05 23:34:35 -0700

    practice update

--- practice.rb
+++ practice.rb
@@ -1,14 +1,12 @@
+=begin
 #Write a program that uses a variable to store a word, then prints that word in reverse order.
 
-=begin
 puts 'type a word: '
 in_word = gets.chomp
 print 'your word in reverse is: ', in_word.reverse, '!'
-=end
 
 #Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
-=begin
 index = 0
 numbers = [3,6,8,4,7]
 while index < numbers.length
@@ -16,11 +14,7 @@ while index < numbers.length
     index=index+1
 end
 
-Write a method that takes in a number and returns the number as a string. Then run the method and print the result.
-Ruby solution
-JavaScript solution
-Python solution
-=end
+#Write a method that takes in a number and returns the number as a string. Then run the method and print the result.
 
 def method1
 
@@ -30,6 +24,20 @@ puts num.to_s
 end
 
 method1 
+
+#Make a hash to store definitions for 2 different words. 
+#Then add a new word and definition and print the hash to see the result.
+
+=end
+
+definitions = {'cat' => 'furry animal','knife' => 'sharp kitchen tool'}
+definitions['chair'] = 'something to sit on'
+
+pp definitions
+
+
+
+
 
 
 
